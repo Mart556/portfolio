2228b7deff0c4dd424c487c9929dac2e9e8f1ea6
Mart556 <marthaamer123@gmail.com>
2025-01-27 23:12:46 +0200

    Scroll animations test and clean up

--- assets/app.js
+++ assets/app.js
@@ -55,29 +55,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const animatedHeader = document.querySelector('.home-animated-header');
     const texts = ['/Junior Software Developer/', 'I build things for the web...'];
     let currentTextIndex = 0;
-    let isDeleting = false;
     let charIndex = 0;
 
     function type() {
         const currentText = texts[currentTextIndex];
-        if (isDeleting) {
-            animatedHeader.textContent = currentText.substring(0, charIndex - 1);
-            charIndex--;
-            if (charIndex === 0) {
-                isDeleting = false;
-                currentTextIndex = (currentTextIndex + 1) % texts.length;
-            }
-        } else {
-            animatedHeader.textContent = currentText.substring(0, charIndex + 1);
-            charIndex++;
-            if (charIndex === currentText.length) {
-                isDeleting = true;
-                setTimeout(type, 5000);
-                return;
-            }
+        animatedHeader.textContent = currentText.substring(0, charIndex + 1);
+        charIndex++;
+        if (charIndex === currentText.length) {
+            charIndex = 0;
+            currentTextIndex = (currentTextIndex + 1) % texts.length;
+            setTimeout(() => {
+                animatedHeader.classList.add('animate__animated', 'animate__fadeOut');
+                setTimeout(() => {
+                    animatedHeader.classList.remove('animate__fadeOut');
+                    animatedHeader.classList.add('animate__fadeIn');
+                    type();
+                }, 1000);
+            }, 5000);
+            return;
         }
-
-        setTimeout(type, isDeleting ? 50 : 100);
+        setTimeout(type, 100);
     }
 
     type();
@@ -116,16 +113,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     Projects.forEach(project => {
         const projectElement = document.createElement('div');
-        projectElement.classList.add('col-4');
+        projectElement.classList.add('col-md-4', 'col-12', 'col-sm-6');
 
-        projectElement.innerHTML = `<div class="p-3 rounded m-2" style="background-color: rgb(28, 37, 65, 0.8); backdrop-filter: blur(5px); max-height: 300px;">	
+        projectElement.innerHTML = `<div class="p-3 rounded m-2 project-box">	
             <div class="row">
                 <div class="col-md-3 col-3">
                     <i class="fa-solid fa-folder fs-2"></i>
                 </div>
 
                 <div class="col-md-6 col-6 text-center">
-                    <h5 class="m-0 fs-4">${project.title}</h5>
+                    <h5 class="m-0 fs-4 project-box-title">${project.title}</h5>
                 </div>
 
                 <div class="col-md-3 col-3 text-end">
