c0d9184e99d9765e0a912a940b87e9cc315d4ef8
Mart556 <marthaamer123@gmail.com>
2025-10-21 18:43:32 +0300

    welcome new, good bye old

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,49 @@
 import type React from "react";
+import { useEffect } from "react";
 
 interface LayoutProps {
 	children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+	useEffect(() => {
+		let lastClickTime = 0;
+
+		document.body.addEventListener("click", function (event) {
+			const target = event.target as HTMLElement;
+			if (
+				window.innerWidth < 768 ||
+				target instanceof HTMLAnchorElement ||
+				target instanceof HTMLButtonElement ||
+				target.closest("button") ||
+				target.closest("a")
+			) {
+				return;
+			}
+
+			const currentTime = new Date().getTime();
+			if (currentTime - lastClickTime < 1000) {
+				return;
+			}
+			lastClickTime = currentTime;
+
+			const hammerSmashGif = document.createElement("img");
+			hammerSmashGif.src = "hammer-smash.gif";
+			hammerSmashGif.style.position = "absolute";
+			hammerSmashGif.style.left = `${event.pageX}px`;
+			hammerSmashGif.style.top = `${event.pageY}px`;
+			hammerSmashGif.style.width = "32px";
+			hammerSmashGif.style.height = "32px";
+			hammerSmashGif.style.pointerEvents = "none";
+			hammerSmashGif.style.zIndex = "9999";
+			document.body.appendChild(hammerSmashGif);
+
+			setTimeout(() => {
+				hammerSmashGif.remove();
+			}, 1000);
+		});
+	}, []);
+
 	return (
 		<div className='flex w-full'>
 			<div className='w-20 shrink-0 hidden md:block' />
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
-		react(/* {
+		react({
 			babel: {
 				plugins: [["babel-plugin-react-compiler"]],
 			},
-		} */),
+		}),
 		tailwindcss(),
+		ViteImageOptimizer({
+			cache: true,
+			png: {
+				quality: 80,
+			},
+			jpeg: {
+				quality: 80,
+			},
+			jpg: {
+				quality: 80,
+			},
+		}),
 	],
 });
